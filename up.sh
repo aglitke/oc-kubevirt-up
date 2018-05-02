c238042977df58e4b6479592eaa7cad7b2a307db
Adam Litke <alitke@redhat.com>
2018-05-02 07:06:38 -0400

    Initial commit
    Signed-off-by: Adam Litke <alitke@redhat.com>

---
+++ up.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+export KUBEVIRT_NS=kube-system
+export CDI_NS=golden-images
+export IP=`ip -o -4 a | tr -s ' ' | grep -v -e ' lo[0-9:]*.*$' | cut -d     ' ' -f 4 | head -1 | sed 's#/.*##'`
+
+#
+# Cluster
+#
+oc cluster up --service-catalog --public-hostname="$IP" --routing-suffix="$IP.nip.io"
+
+oc login -u system:admin --insecure-skip-tls-verify=true
+
+#
+# Storage
+#
+oc get serviceaccount storage-demo -n $KUBEVIRT_NS || \
+	oc create serviceaccount storage-demo -n $KUBEVIRT_NS
+oc adm policy add-scc-to-user privileged system:serviceaccount:$KUBEVIRT_NS:storage-demo
+oc new-project $CDI_NS
+oc get clusterrolebinding c-$CDI_NS-default || \                    
+	oc create clusterrolebinding c-$CDI_NS-default --clusterrole=cluster-admin --serviceaccount=$CDI_NS:default 
+
+rm _out/*
+j2 -f env manifests/storage-demo.yml.j2 > _out/storage-demo.yml
+j2 -f env manifests/cdi-resourcequota.yml.j2 > _out/cdi-resourcequota.yml
+j2 -f env manifests/cdi-controller-deployment.yml.j2 > _out/cdi-controller-deployment.yml
+
+oc create -f _out/storage-demo.yml
+oc create -f _out/cdi-resourcequota.yml
+oc create -f _out/cdi-controller-deployment.yml
+
+#
+# Kubevirt
+#
+oc adm policy add-scc-to-user privileged -z kubevirt-privileged -n $KUBEVIRT_NS
+oc adm policy add-scc-to-user privileged -z kubevirt-controller -n $KUBEVIRT_NS
+oc adm policy add-scc-to-user privileged -z kubevirt-infra -n $KUBEVIRT_NS
+oc create -f manifests/kubevirt.yaml
+
+#                                                                               
+# Misc                                                                          
+#                                                                               
+oc adm policy add-cluster-role-to-user cluster-admin developer                  
+oc patch -n openshift-web-console  deploy webconsole -p '{"spec":{"template":{"spec":{"containers":[{"name": "webconsole","image": "mutism/origin-web-console:demo"}]}}}}'  
+
+#
+# ASB
+#
+./service-catalog/run_latest_build.sh                                           
+oc delete -f service-catalog/broker-config.yml                                  
+oc create -f service-catalog/broker-config.yml                                  
+oc delete pod -n ansible-service-broker -l app=ansible-service-broker           
+
